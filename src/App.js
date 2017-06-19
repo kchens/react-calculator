c01d2096698de9f4ca7106610051cc4460a3c671
Kevin Chen <kevin.kang.chen@gmail.com>
2017-06-19 10:48:17 -0700

    CODE NOT WORKING: CONVERTING TO REACT

--- src/App.js
+++ src/App.js
@@ -9,31 +9,58 @@ const POS_NEG_SIGN = '+/-'
 const NEG_SIGN = '-'
 const PERCENT_SIGN = '%'
 
+const DIVIDE_OPERATOR   = '÷'
+const MULTIPLY_OPERATOR = 'x'
+const MINUS_OPERATOR    = '-'
+const ADD_OPERATOR      = '+'
+const EQUAL_OPERATOR    = '='
+
 function parseInt10(targetText) {
   const radix = 10
   return parseInt(targetText, radix)
 }
 
-function handleAllClickEvensts(e) {
-  const currentTotal = document.getElementById(TOTAL_ID).innerText
-  const targetValue = e.target.innerText
-  let newTotal
-  let hasHitPercent = document.getElementById('hasHitPercent').innerText
-
-  if (currentTotal.indexOf(DECIMAL_SIGN) > -1 && targetValue === DECIMAL_SIGN) {
-    return
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasHitPercent: false,
+      equation: [],
+      currentTotal: 0,
+    }
+    this.handleAllClickEvents = this.handleAllClickEvents.bind(this)
+    this.togglePosNegSign = this.togglePosNegSign.bind(this)
   }
 
-  // e.target.className.indexOf('w-25')
+  togglePosNegSign() {
 
-  if (!isNaN(parseInt10(targetValue)) || targetValue === DECIMAL_SIGN) {
-    if (currentTotal === '0' || currentTotal === '-0' || hasHitPercent === 'true') {
-      document.getElementById('hasHitPercent').innerHTML = 'false'
-      return document.getElementById(TOTAL_ID).innerHTML = targetValue
-    } else {
-      return document.getElementById(TOTAL_ID).innerHTML = currentTotal.concat(targetValue)
+  }
+
+  handleAllClickEvents(e) {
+    const currentTotal = document.getElementById(TOTAL_ID).innerText
+    const targetValue = e.target.innerText
+    let equation = [currentTotal]
+    let newTotal
+
+    if (currentTotal.indexOf(DECIMAL_SIGN) > -1 && targetValue === DECIMAL_SIGN) {
+      return
     }
-  } else {
+
+    // e.target.className.indexOf('w-25')
+    debugger
+    if (!isNaN(parseInt10(targetValue)) || targetValue === DECIMAL_SIGN) {
+      if (currentTotal === '0' || currentTotal === '-0' || this.state.hasHitPercent === false) {
+        // this.setState( (prevState, props) => {return { hasHitPercent: false } })
+
+        this.setState({
+          hasHitPercent: true
+        })
+        newTotal = targetValue
+      } else {
+        newTotal = currentTotal.concat(targetValue)
+      }
+      return document.getElementById(TOTAL_ID).innerHTML = newTotal
+    }
 
     switch (targetValue) {
       case AC_TEXT:
@@ -48,56 +75,66 @@ function handleAllClickEvensts(e) {
         break
       case PERCENT_SIGN:
         // stretch: if there is an 'e', then display at most 5 decimal points
-        // must: if percent_sign has been hit once, then must clear on next number. need state? hidden tag?
-
-        if (hasHitPercent === 'false') {
-          hasHitPercent = 'true'
-          document.getElementById('hasHitPercent').innerHTML = hasHitPercent
+        if (this.state.hasHitPercent === false) {
+          this.setState( { hasHitPercent: true } )
         }
-
         newTotal = currentTotal / 100
+
+        break
+      case DIVIDE_OPERATOR:
+        // document.getElementById(TOTAL_ID).innerHTML = currentTotal.concat()
+        break
+      case MULTIPLY_OPERATOR:
+        break
+      case MINUS_OPERATOR:
+        break
+      case ADD_OPERATOR:
+        equation.push(ADD_OPERATOR)
+        newTotal = currentTotal
+        break
+      case EQUAL_OPERATOR:
+        newTotal = eval(equation.join(''))
         break
       default:
         newTotal = currentTotal
     }
+    debugger
+    return document.getElementById(TOTAL_ID).innerHTML = newTotal
   }
-  document.getElementById(TOTAL_ID).innerHTML = newTotal
-}
 
-class App extends Component {
   render() {
     return (
-      <div className="w-50" onClick={handleAllClickEvensts}>
+      <div className="w-50" onClick={this.handleAllClickEvents}>
         <div id={TOTAL_ID} className="white bg-black tr">0</div>
-        <div id="hasHitPercent" hidden>false</div>
+        <div id="operator" hidden>false</div>
         <div>
           <div className="fl w-25 tc bg-light-silver">{AC_TEXT}</div>
           <div className="fl w-25 tc bg-light-silver">{POS_NEG_SIGN}</div>
-          <div className="fl w-25 tc bg-light-silver">%</div>
-          <div className="fl w-25 tc bg-orange white">÷</div>
+          <div className="fl w-25 tc bg-light-silver">{PERCENT_SIGN}</div>
+          <div className="fl w-25 tc bg-orange white">{DIVIDE_OPERATOR}</div>
         </div>
         <div>
           <div className="fl w-25 tc bg-light-gray">7</div>
           <div className="fl w-25 tc bg-light-gray">8</div>
           <div className="fl w-25 tc bg-light-gray">9</div>
-          <div className="fl w-25 tc bg-orange white">x</div>
+          <div className="fl w-25 tc bg-orange white">{MULTIPLY_OPERATOR}</div>
         </div>
         <div>
           <div className="fl w-25 tc bg-light-gray">4</div>
           <div className="fl w-25 tc bg-light-gray">5</div>
           <div className="fl w-25 tc bg-light-gray">6</div>
-          <div className="fl w-25 tc bg-orange white">-</div>
+          <div className="fl w-25 tc bg-orange white">{MINUS_OPERATOR}</div>
         </div>
         <div>
           <div className="fl w-25 tc bg-light-gray">1</div>
           <div className="fl w-25 tc bg-light-gray">2</div>
           <div className="fl w-25 tc bg-light-gray">3</div>
-          <div className="fl w-25 tc bg-orange white">+</div>
+          <div className="fl w-25 tc bg-orange white">{ADD_OPERATOR}</div>
         </div>
         <div>
           <div className="fl w-50 tl bg-light-gray pl4">0</div>
           <div className="fl w-25 tc bg-light-gray">{DECIMAL_SIGN}</div>
-          <div className="fl w-25 tc bg-orange white">=</div>
+          <div className="fl w-25 tc bg-orange white">{EQUAL_OPERATOR}</div>
         </div>
       </div>
     );
