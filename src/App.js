f805ff16a7ab255f91ce2f0c19b19234c423ffb7
Kevin Chen <kevin.kang.chen@gmail.com>
2017-06-20 00:44:29 -0700

    feat(react-evals): evaluates addition correctly

--- src/App.js
+++ src/App.js
@@ -9,8 +9,12 @@ const POS_NEG_SIGN = '+/-'
 const NEG_SIGN = '-'
 const PERCENT_SIGN = '%'
 
-const DIVIDE_OPERATOR   = '÷'
-const MULTIPLY_OPERATOR = 'x'
+const DIVIDE_SIGN       = '÷'
+const DIVIDE_OPERATOR   = '/'
+
+const MULTIPLY_SIGN     = 'x'
+const MULTIPLY_OPERATOR = '*'
+
 const MINUS_OPERATOR    = '-'
 const ADD_OPERATOR      = '+'
 const EQUAL_OPERATOR    = '='
@@ -20,18 +24,22 @@ class App extends Component {
     super(props)
     this.state = {
       hasHitPercent: false,
-      equation: [],
       currentTotal: '0',
+      lastTotal: '0',
+      displayTotal: '0',
+      operator: null,
+      hasHitOperator: false,
     }
     this.togglePosNegSign = this.togglePosNegSign.bind(this)
-    this.add = this.add.bind(this)
+    this.setOperator = this.setOperator.bind(this)
     this.concatTotal = this.concatTotal.bind(this)
     this.clearTotal = this.clearTotal.bind(this)
     this.makePercent = this.makePercent.bind(this)
+    this.evaluateEquation = this.evaluateEquation.bind(this)
   }
 
   clearTotal() {
-    this.setState({currentTotal: '0'})
+    this.setState({currentTotal: '0', displayTotal: '0'})
   }
 
   concatTotal(e) {
@@ -52,28 +60,48 @@ class App extends Component {
     } else {
       newString = currentTotal.concat(targetText)
     }
-    this.setState({currentTotal: newString})
+    this.setState({currentTotal: newString, displayTotal: newString})
   }
 
   togglePosNegSign() {
     const { currentTotal } = this.state
-    let newTotal
+    let newString
     if (currentTotal.indexOf(NEG_SIGN) > -1) {
-      newTotal = currentTotal.substring(1, currentTotal.length)
+      newString = currentTotal.substring(1, currentTotal.length)
     } else {
-      newTotal = NEG_SIGN.concat(currentTotal)
+      newString = NEG_SIGN.concat(currentTotal)
     }
-    this.setState({ currentTotal: newTotal})
+    this.setState({ currentTotal: newString, displayTotal: newString})
   }
 
   makePercent() {
-    const newTotal = parseFloat(this.state.currentTotal) / 100
-    this.setState({currentTotal: "" + newTotal})
+    let newTotal = parseFloat(this.state.currentTotal) / 100
+    newTotal = "" + newTotal
+    this.setState({currentTotal: newTotal, displayTotal: newTotal})
   }
 
-  add(e) {
-    // debugger
-    // this.setState({ currentTotal: this.state.currentTotal + parseInt10(e.target.innerText)})
+  setOperator(e) {
+    const { currentTotal, displayTotal } = this.state
+    let newOperator
+    switch (e.target.innerText) {
+      case MULTIPLY_SIGN:
+        newOperator = MULTIPLY_OPERATOR
+      case DIVIDE_SIGN:
+        newOperator = DIVIDE_OPERATOR
+      default:
+        newOperator = e.target.innerText
+    }
+
+    this.setState({operator: newOperator, lastTotal: displayTotal, currentTotal: '0'})
+  }
+
+  evaluateEquation() {
+    const { lastTotal, currentTotal, operator } = this.state
+    let newTotal
+    newTotal = eval(lastTotal + operator + currentTotal)
+    console.log(this.state)
+    console.log(newTotal)
+    this.setState({lastTotal: newTotal, displayTotal: newTotal})
   }
 
   _hasDecimal(currentTotal, targetText) {
@@ -83,18 +111,18 @@ class App extends Component {
   render() {
     return (
       <div className="w-50">
-        <div id={TOTAL_ID} className="white bg-black tr">{this.state.currentTotal}</div>
+        <div id={TOTAL_ID} className="white bg-black tr">{this.state.displayTotal}</div>
         <div>
           <div className="fl w-25 tc bg-light-silver" onClick={this.clearTotal}>{AC_TEXT}</div>
           <div className="fl w-25 tc bg-light-silver" onClick={this.togglePosNegSign}>{POS_NEG_SIGN}</div>
           <div className="fl w-25 tc bg-light-silver" onClick={this.makePercent}>{PERCENT_SIGN}</div>
-          <div className="fl w-25 tc bg-orange white">{DIVIDE_OPERATOR}</div>
+          <div className="fl w-25 tc bg-orange white">{DIVIDE_SIGN}</div>
         </div>
         <div>
-          <div className="fl w-25 tc bg-light-gray" onClick={this.add} data-value={7}>7</div>
+          <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>7</div>
           <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>8</div>
           <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>9</div>
-          <div className="fl w-25 tc bg-orange white">{MULTIPLY_OPERATOR}</div>
+          <div className="fl w-25 tc bg-orange white">{MULTIPLY_SIGN}</div>
         </div>
         <div>
           <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>4</div>
@@ -106,12 +134,12 @@ class App extends Component {
           <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>1</div>
           <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>2</div>
           <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>3</div>
-          <div className="fl w-25 tc bg-orange white" onClick={this.add}>{ADD_OPERATOR}</div>
+          <div className="fl w-25 tc bg-orange white" onClick={this.setOperator}>{ADD_OPERATOR}</div>
         </div>
         <div>
           <div className="fl w-50 tl bg-light-gray pl4" onClick={this.concatTotal}>0</div>
           <div className="fl w-25 tc bg-light-gray" onClick={this.concatTotal}>{DECIMAL_SIGN}</div>
-          <div className="fl w-25 tc bg-orange white">{EQUAL_OPERATOR}</div>
+          <div className="fl w-25 tc bg-orange white" onClick={this.evaluateEquation}>{EQUAL_OPERATOR}</div>
         </div>
       </div>
     );
